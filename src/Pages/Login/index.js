24d9a549eadbec8f4a9fbed0ed61ccac376d03d4
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2022-08-02 10:06:22 -0300

    flexbox to the moon!!

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,27 +1,24 @@
 import React from "react";
 
-import { Login } from "./styled";
+import { Login, PageArea } from "./styled";
 
 import { ErrorMessage } from "../../components/MainComponents";
 
 const Page = () => {
     return (
-        <Login>
-            <div className="container">
-                <label>pollen</label>
-                <form>
-                    <input 
-                        type="email"    
-                    />
-                    <input 
-                        type="password"    
-                    />
-                    <button>entrar</button>
-                </form>
-            </div>
-        </Login>
-        
+        <PageArea>
+            <Login>
+                <div className="container">
+                    <label>pollen</label>
+                    <form>
+                        <input type="email" />
+                        <input type="password" />
+                        <button>entrar</button>
+                    </form>
+                </div>
+            </Login>
+        </PageArea>
     );
-}
+};
 
 export default Page;
--- src/Pages/Login/styled.js
+++ src/Pages/Login/styled.js
@@ -1,27 +1,24 @@
 import styled from "styled-components";
 
+export const PageArea = styled.div`
+    --primary-color: #141414;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--primary-color);
+`;
+
 export const Login = styled.div`
-
-    .container{
-        /* position: relative;
-        width: 451px;
-        height: 561px;
-        background: #242424; */
-
-        box-sizing: border-box;
-
-        position: absolute;
-        width: 451px;
-        height: 561px;
-        left: 495px;
-        top: 232px;
+    .container {
+        width: 450px;
+        height: 560px;
 
         background: #242424;
         border-width: 1px 3px 3px 1px;
         border-style: solid;
         border-color: #000000;
         border-radius: 150px 0px;
-
-
     }
 `;
