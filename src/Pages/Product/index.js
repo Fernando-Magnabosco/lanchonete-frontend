fefccf7d62cbee2bfd0099fe68244f1e8d2a886e
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2022-08-04 11:52:13 -0300

    pagina products funcionando;

--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -1,45 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import MaskedInput from 'react-text-mask';
-import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import MaskedInput from "react-text-mask";
+import createNumberMask from "text-mask-addons/dist/createNumberMask";
 
-import useApi from '../../helpers/api';
+import useApi from "../../helpers/api";
 
 import { PageArea, Estilo } from "./styled";
 import { ErrorMessage } from "../../components/mainComponents";
 
 const Page = () => {
-
     const api = useApi();
 
     const [categories, setCategories] = useState([]);
-    const [title, setTitle] = useState('');
-    const [category, setCategory] = useState('');
-    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [price, setPrice] = useState("");
     const [priceNegtiable, setPriceNegtiable] = useState(false);
-    const [desc, setDesc] = useState('');
+    const [desc, setDesc] = useState("");
 
     const [disabled, setDisabled] = useState(false);
-    const [error, setError] = useState('');
+    const [error, setError] = useState("");
 
-    useEffect(()=>{
-        const getCategories = async ()=> {
+    useEffect(() => {
+        const getCategories = async () => {
             const cats = await api.getCategories();
             console.log(cats);
-            setCategories(cats);
-        }
+            setCategories(cats.categories);
+        };
         getCategories();
-    },[]);
+    }, []);
 
     const priceMask = createNumberMask({
-        prefix: 'R$ ',
+        prefix: "R$ ",
         includeThousandsSeparator: true,
-        thousandsSeparatorSymbol: '.',
+        thousandsSeparatorSymbol: ".",
         allowDecimal: true,
-        decimalSymbol: ','
+        decimalSymbol: ",",
     });
-
-
 
     return (
         <PageArea>
@@ -49,11 +46,11 @@ const Page = () => {
                     <form>
                         <div className="area">
                             <h3>Nome:</h3>
-                            <input 
+                            <input
                                 type="text"
                                 disabled={disabled}
                                 value={title}
-                                onChange={e=>setTitle(e.target.value)}
+                                onChange={(e) => setTitle(e.target.value)}
                             />
                         </div>
 
@@ -61,11 +58,19 @@ const Page = () => {
                             <h3>Categoria:</h3>
                             <select
                                 disabled={disabled}
-                                onChange={e=>setCategory(e.target.value)}
-                                required                            
+                                onChange={(e) => setCategory(e.target.value)}
+                                required
                             >
                                 <option value="">Selecione</option>
-                                {categories && categories.map(i=><option key={i._id} value={i._id}>{i.name}</option>)}
+                                {categories &&
+                                    categories.map((i) => (
+                                        <option
+                                            key={i.id_categoria}
+                                            value={i.id_categoria}
+                                        >
+                                            {i.nm_categoria}
+                                        </option>
+                                    ))}
                             </select>
                         </div>
 
@@ -75,18 +80,14 @@ const Page = () => {
                                 mask={priceMask}
                                 placeholder="R$"
                                 disabled={disabled || priceNegtiable}
-                                onChange={e=>setPrice(e.target.value)}
+                                onChange={(e) => setPrice(e.target.value)}
                             />
                         </div>
 
                         <div className="area">
                             <h3>Imagem do produto:</h3>
-                            <input
-                                type="file"
-                                multiple
-                            />
+                            <input type="file" multiple />
                         </div>
-
 
                         <button>cadastrar</button>
                     </form>
