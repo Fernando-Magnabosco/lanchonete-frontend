74ffc4a63daee9a681d4b676085e636a34325c85
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2022-08-04 08:51:45 -0300

    api created;

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,95 +1,87 @@
 import React, { useState } from "react";
-import useApi from '../../helpers/api'
+import useApi from "../../helpers/api";
 import { Login, PageArea } from "./styled";
-import { doLogin } from "../../helpers/AuthHandler"
+import { doLogin } from "../../helpers/authHandler";
 import { ErrorMessage } from "../../components/MainComponents";
 
 const Page = () => {
-
     const api = useApi();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [rememberPassword, setrememberPassword] = useState(false);
     const [disabled, setDisabled] = useState(false);
-    const [error, setError] = useState('');
+    const [error, setError] = useState("");
 
     const handleSubmit = async (e) => {
-        
-
         e.preventDefault();
         setDisabled(true);
 
         const json = await api.login(email, password);
 
-        console.log('entrou');
-        console.log(json);
-
-        if(json.error){
+        if (json.error) {
             setError(json.error);
         } else {
             doLogin(json.token, rememberPassword);
-            window.Location.href = '/home';
+            window.Location.href = "/home";
         }
-
+        setDisabled(false);
     };
 
     return (
         <PageArea>
             <Login>
-                
                 <div className="container">
                     <div className="area-title">
                         <h2>pollen</h2>
-                        <hr/>
+                        <hr />
                     </div>
 
-                    {error && 
-                        <ErrorMessage>{error}</ErrorMessage>
-                    }
-                    
-                    <form onSubmit={handleSubmit} >
-                        
+                    {error && <ErrorMessage>{error}</ErrorMessage>}
+
+                    <form onSubmit={handleSubmit}>
                         <label className="area">
-                        <span>E-mail</span><br/>
-                        <input 
-                            type="email" 
-                            placeholder="e-mail"
-                            disabled={disabled}
-                            value={email}
-                            onChange={e=>setEmail(e.target.value)}
-                            required
-                        />
+                            <span>E-mail</span>
+                            <br />
+                            <input
+                                type="email"
+                                placeholder="e-mail"
+                                disabled={disabled}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
                         </label>
-                        
+
                         <label className="area">
-                            <span>Senha</span><br/>
-                        <input 
-                            type="password" 
-                            placeholder="senha"
-                            disabled={disabled}    
-                            value={password}
-                            onChange={e=>setPassword(e.target.value)}
-                            required
-                        />
+                            <span>Senha</span>
+                            <br />
+                            <input
+                                type="password"
+                                placeholder="senha"
+                                disabled={disabled}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
                         </label>
 
                         <label className="area-lembrar">
                             <h3>lembrar a senha? </h3>
                             <div id="abc">
-                                <span  className="marcar">
-                            </span>
-                            <input 
-                                type="checkbox"
-                                disabled={disabled}
-                                checked={rememberPassword}
-                                onChange={()=>setrememberPassword(!rememberPassword)}
-                            />
+                                <span className="marcar"></span>
+                                <input
+                                    type="checkbox"
+                                    disabled={disabled}
+                                    checked={rememberPassword}
+                                    onChange={() =>
+                                        setrememberPassword(!rememberPassword)
+                                    }
+                                />
                             </div>
                         </label>
 
-                        
-                        <button disabled={disabled} >entrar</button>
+                        <button disabled={disabled}>entrar</button>
                     </form>
                 </div>
             </Login>
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,13 @@
 import React from "react";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Login from './Pages/Login';
+import Login from "./Pages/Login";
 
 export default () => {
     return (
         <Routes>
-            
-            <Route path="/login" element={<Login/>}/>
-            
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={<Login />} />
         </Routes>
     );
-}
+};
