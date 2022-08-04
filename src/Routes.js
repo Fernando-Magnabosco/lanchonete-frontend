6a893fb174191f622273a8d13438b4849786802c
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2022-08-04 09:39:31 -0300

    routes fixed;

--- src/Routes.js
+++ src/Routes.js
@@ -1,33 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 import Home from "./Pages/Home";
-=======
-<<<<<<< HEAD
-import Login from './Pages/Login';
-import Product from './Pages/Product';
-=======
->>>>>>> 6d1f63fc1e54a5a231fe1e286f4165136ef2a172
 import Login from "./Pages/Login";
->>>>>>> b3270d5ca0671ab5784431e4df6ba496b729e331
+import Product from "./Pages/Product";
 
 export default () => {
     return (
         <Routes>
-<<<<<<< HEAD
             <Route path="/" element={<Home />} />
-=======
-<<<<<<< HEAD
-            
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/product" element={<Product/>}/>
-            
-=======
->>>>>>> 6d1f63fc1e54a5a231fe1e286f4165136ef2a172
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/product" element={<Product />} />
             <Route path="/login" element={<Login />} />
             <Route path="/*" element={<Login />} />
->>>>>>> b3270d5ca0671ab5784431e4df6ba496b729e331
         </Routes>
     );
 };
