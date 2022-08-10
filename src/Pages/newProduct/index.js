ddf90d1c7f0a3cca904bb1a943eb1303c3a5ce09
Anthony <anthonywendyantunesdelima@gmail.com>
2022-08-10 11:58:15 -0300

    Caixa de seleção de inputs criada

--- src/Pages/newProduct/index.js
+++ src/Pages/newProduct/index.js
@@ -8,6 +8,14 @@ import useApi from "../../helpers/api";
 import { PageArea } from "./styled";
 import { ErrorMessage } from "../../components/mainComponents";
 
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+
 const Page = () => {
     const api = useApi();
     const fileField = useRef();
@@ -77,6 +85,8 @@ const Page = () => {
         setDisabled(false);
     };
 
+    const names = [];
+
     const priceMask = createNumberMask({
         prefix: "R$ ",
         includeThousandsSeparator: true,
@@ -84,6 +94,31 @@ const Page = () => {
         allowDecimal: true,
         decimalSymbol: ",",
     });
+
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+    PaperProps: {
+        style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+        },
+    },
+    };
+
+    
+    const [personName, setPersonName] = useState([]);
+    
+    const handleChange = (event) => {
+        const {
+        target: { value },
+        } = event;
+        setPersonName(
+        // On autofill we get a stringified value.
+        typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+    
 
     return (
         <PageArea>
@@ -159,6 +194,29 @@ const Page = () => {
                                 multiple
                             />
                         </label>
+                    </div>
+
+                    <div>
+                        <FormControl sx={{ m: 1, width: 300 }}>
+                            <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+                            <Select
+                            labelId="demo-multiple-checkbox-label"
+                            id="demo-multiple-checkbox"
+                            multiple
+                            value={personName}
+                            onChange={handleChange}
+                            input={<OutlinedInput label="Tag" />}
+                            renderValue={(selected) => selected.join(', ')}
+                            MenuProps={MenuProps}
+                            >
+                            {names.map((name) => (
+                                <MenuItem key={name} value={name}>
+                                <Checkbox checked={personName.indexOf(name) > -1} />
+                                <ListItemText primary={name} />
+                                </MenuItem>
+                            ))}
+                            </Select>
+                        </FormControl>
                     </div>
 
                     <div className="area">
