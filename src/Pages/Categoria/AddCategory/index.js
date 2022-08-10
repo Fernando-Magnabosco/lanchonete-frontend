e46eee7c635834ef8e02c0bf97cee50ff7bfaade
Anthony <anthonywendyantunesdelima@gmail.com>
2022-08-10 08:39:53 -0300

    Created page "AddCategory"

---
+++ src/Pages/Categoria/AddCategory/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import useApi from "../../../helpers/api";
+import { PageArea } from "./styled";
+import { ErrorMessage } from "../../../components/mainComponents";
+
+const Page = () => {
+
+    return (
+        <PageArea>
+                        
+        </PageArea>
+
+    );
+};
+export default Page;
